f339c6a383bc396ff2d162750e16ab17a5a4f9da
unknown <cubamc@gmail.com>
2019-05-01 23:55:10 -0400

    heroku fixed

--- src/utils/loadMusic.js
+++ src/utils/loadMusic.js
@@ -1,13 +1,21 @@
 import axios from 'axios'
 
+let url = 'https://pxmusic.herokuapp.com'
+    if(process.env.NODE_ENV === "development"){
+        url = 'http://localhost:3000'
+    }
+
+
 export const loadAllMusic = async () => {
-    const res = await axios.get(`http://localhost:3000/api/music/all/`)
+    
+
+    const res = await axios.get(`${url}/api/music/all/`)
     return res.data
    
 }
 
 export const loadMusic = async (page = 1, limit = 1) => {
-    const res = await axios.get(`http://localhost:3000/api/music/all?page=${page}&limit=${limit}`)
+    const res = await axios.get(`${url}/api/music/all?page=${page}&limit=${limit}`)
     return res.data
    
 }
